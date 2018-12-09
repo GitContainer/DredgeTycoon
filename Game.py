fb0cd42989a30081e64037c2b0d536327856c0b6
rcriii42 <rcriii@ramsdells.net>
2018-12-09 16:53:35 -0600

    Move the Player object into its own file

--- Game.py
+++ Game.py
@@ -1,10 +1,9 @@
 """Game.py - Contains the Game object that holds all objects in the game - players, cities, 
    dredges, and the game world."""
 # Model 
-import os, random, datetime
-import pygame
+import datetime
 from City import City
-from Dredge import Dredge
+from Player import Player
 Demo=True
 days_to_hours = 24.
 
@@ -13,53 +12,6 @@ def comprehension_flatten(iter_lst):
     return list(item for iter_ in iter_lst for item in iter_)
 
 
-class Player(object):
-    """Player with dredges"""
-    def __init__(self, name="First Player"):
-        self.name = name
-        self.dredges = []
-        self.value=1000000
-        self.projects = [] # a list of Projects
-        if Demo:
-            self.dredges.append(Dredge(name="Treasure Island", owner=self, production=2000, loc=(5+16,425+16)))
-            self.dredges.append(Dredge(owner=self,loc=(400,400)))
-    
-    def generateDredge(self, name=None):
-        # add a dredge to a player
-        #What purpose does this function serve?
-        self.dredges.append(Dredge())  
-    
-    def TimeStep(self, steptimesize, game):
-        """Adjust the player to reflect moving forward in time.
-           steptimesize in the time step in days.
-           game is the game I am part of."""
-        ts_messages=[]
-        revenue = 0
-        for myproject in self.projects:
-            # For Profit - expect projects to be updated before players
-            revenue += myproject.quantityThisPeriod * myproject.unitcost
-        fixed_costs = 0
-        working_costs = 0
-        for mydredge in self.dredges:
-            # For Payroll! wait Boo!
-            fixed_costs -= mydredge.fixedCosts*steptimesize
-            mydredge.TimeStep(steptimesize, game)
-            city_its_in = [c for c in game.cities if mydredge.in_city(c)]
-            if mydredge.assigned_project and city_its_in:
-                working_costs -= mydredge.workingCosts*steptimesize
-            elif city_its_in:
-                projects = [p for p in city_its_in[0].projects if p.player == self]
-                if projects:
-                    mydredge.assigned_project = projects[0]
-                    mydredge.assigned_project.dredges.append(mydredge)
-                    ts_messages.append((mydredge,"%s assigned to %s"%(mydredge.name, mydredge.assigned_project)))
-        self.value += (revenue + fixed_costs + working_costs)
-        return ts_messages
-
-
-
-
-        
 class Game(object):
     """Holds the various game objects"""
     def __init__(self):
---
+++ Player.py
@@ -0,0 +1,48 @@
+"""Player.py - Contsains the Player object"""
+from Dredge import Dredge
+import Game
+
+
+class Player(object):
+    """Player with dredges"""
+
+    def __init__(self, name="First Player"):
+        self.name = name
+        self.dredges = []
+        self.value = 1000000
+        self.projects = []  # a list of Projects
+        if Game.Demo:
+            self.dredges.append(Dredge(name="Treasure Island", owner=self, production=2000, loc=(5 + 16, 425 + 16)))
+            self.dredges.append(Dredge(owner=self, loc=(400, 400)))
+
+    def generateDredge(self, name=None):
+        # add a dredge to a player
+        # What purpose does this function serve?
+        self.dredges.append(Dredge())
+
+    def TimeStep(self, steptimesize, game):
+        """Adjust the player to reflect moving forward in time.
+           steptimesize in the time step in days.
+           game is the game I am part of."""
+        ts_messages = []
+        revenue = 0
+        for myproject in self.projects:
+            # For Profit - expect projects to be updated before players
+            revenue += myproject.quantityThisPeriod * myproject.unitcost
+        fixed_costs = 0
+        working_costs = 0
+        for mydredge in self.dredges:
+            # For Payroll! wait Boo!
+            fixed_costs -= mydredge.fixedCosts * steptimesize
+            mydredge.TimeStep(steptimesize, game)
+            city_its_in = [c for c in game.cities if mydredge.in_city(c)]
+            if mydredge.assigned_project and city_its_in:
+                working_costs -= mydredge.workingCosts * steptimesize
+            elif city_its_in:
+                projects = [p for p in city_its_in[0].projects if p.player == self]
+                if projects:
+                    mydredge.assigned_project = projects[0]
+                    mydredge.assigned_project.dredges.append(mydredge)
+                    ts_messages.append((mydredge, "%s assigned to %s" % (mydredge.name, mydredge.assigned_project)))
+        self.value += (revenue + fixed_costs + working_costs)
+        return ts_messages
