a371b33fab3374e5f77662156caa9d59f8a43e3c
rcriii42 <rcriii@ramsdells.net>
2018-12-23 12:14:54 -0600

    Add Norfolk to city list
    Move Boston and New York slightly

--- Game.py
+++ Game.py
@@ -4,7 +4,8 @@
 import datetime
 from City import City
 from Player import Player
-Demo=True
+
+Demo = True
 days_to_hours = 24.
 
 
@@ -14,21 +15,25 @@ def comprehension_flatten(iter_lst):
 
 class Game(object):
     """Holds the various game objects"""
+
     def __init__(self):
         self.date = datetime.datetime(1920, 1, 1)
-        self.cities = [City(self.date, "Boston", (375,20)),
-                       City(self.date, "New York", (400,80)),
-                       City(self.date, "Jacksonville", (5,425)),
+
+        self.cities = [City(self.date, "Boston", (450, 20)),
+                       City(self.date, "New York", (375, 80)),
+                       City(self.date, "Jacksonville", (5, 425)),
+                       City(self.date, "Norfolk", (300, 200))
                        ]
-        self.players = [Player()]    
-        self.date = datetime.datetime(1920,1,1)
-    
+
+        self.players = [Player()]
+        self.date = datetime.datetime(1920, 1, 1)
+
     @property
     def dredges(self):
         """return a list of all dredges"""
-        return comprehension_flatten([p.dredges for p in self.players]) # this returns a flat list of all dredges
-    
+        return comprehension_flatten([p.dredges for p in self.players])  # this returns a flat list of all dredges
+
     @property
     def projects(self):
         """return a list of projects"""
-        return comprehension_flatten([c.projects for c in self.cities]) # this returns a flat list of projects underway
+        return comprehension_flatten([c.projects for c in self.cities])  # this returns a flat list of projects underway
